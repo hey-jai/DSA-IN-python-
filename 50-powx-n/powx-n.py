ad5ec52d403f7929e858b14fe7af642f270019e8
JAI RAJ <uttam853993@gmail.com>
2026-08-09 20:03:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (28.04%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -5,5 +5,13 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        return x**n
+        if n==0 :
+            return 1
+        if n<0 :
+            return  1/self.myPow(x,-n) 
+        if n%2==0 :
+            return self.myPow(x*x,n//2)
+        else :
+            return  x * self.myPow(x*x,n//2)
+
         
